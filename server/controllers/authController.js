0871e8ff353fec87ea2917e6dbf3ccd35764b138
mukeshkumar99422 <156451966+mukeshkumar99422@users.noreply.github.com>
2026-03-12 21:33:44 +0530

    updated backend for vercel

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -277,7 +277,9 @@ const changePassword = async (req, res) => {
 const logout = (req, res) => {
     res.cookie('token', '', {
         httpOnly: true,
-        expires: new Date(0), // Expire immediately
+        secure: true,
+        sameSite: 'none',
+        expires: new Date(0)
     });
     res.status(200).json({ message: 'Logged out successfully' });
 };
