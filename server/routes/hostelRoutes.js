fbd61d0770d1fd952e63f5e89a564dda671706d7
mukeshkumar99422 <156451966+mukeshkumar99422@users.noreply.github.com>
2026-07-25 00:29:41 +0530

    error fixed in hostel route

--- server/routes/hostelRoutes.js
+++ server/routes/hostelRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { getAllHostels } = require('../controllers/hostelController');
 const { publicReadLimiter } = require('../middlewares/rateLimiter');
 const { cacheResponse, keys } = require('../middlewares/cacheMiddleware');
-const {}
 
 // Route: GET /api/hostels
 router.get('/', publicReadLimiter, cacheResponse(()=> keys.hostelsPublicList(),3600), getAllHostels);
